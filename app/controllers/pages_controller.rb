0552b082c3161f3e7b16afd0609fab90ca4ca7d7
Timur Meyster <timur.meyster@gmail.com>
2014-01-12 13:49:51 -0500

    Customize bootstrap variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def about 
   end
+
+  def team 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pintresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "team" => "pages#team"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
