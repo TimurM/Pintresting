937837236417a6d90a6ffa8ccb4aeef52efdf5ef
Timur Meyster <timur.meyster@gmail.com>
2014-01-14 00:56:27 -0500

    Cleaned up the pins scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pintresting::Application.routes.draw do
+  resources :pins
+
   devise_for :users
   root "pages#home"
   get "about" => "pages#about"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140112200458) do
+ActiveRecord::Schema.define(version: 20140114043011) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
