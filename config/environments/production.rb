d62ccd4caa225f44b663f4087af0d53506fa9d83
Timur Meyster <timur.meyster@gmail.com>
2014-01-16 00:20:02 -0500

    Add amazon S3 for paperclip uploads to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,14 @@ Pintresting::Application.configure do
   #Required for Heroku 
   #Note to set this to your actual host name
   config.action_mailer.default_url_options = { :host => 'http://rentalz-pintresting.herokuapp.com' }
+
+  #paperclip on amazon S3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
